be944abc13a4a78f8e4633c4d2d514aa5a15ebec
IgnisIncendio <IgnisIncendio@protonmail.com>
2017-07-08 19:07:31 +0800

    Add UFOs

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour {
-    public enum State { IDLE, ASTEROID };
+    private float timePassedSinceLastStateChange = 0;
+    public enum State { IDLE, ASTEROID, GREEN_UFO };
     public State state = State.IDLE;
 
     public float enemyZ = -1;
@@ -11,30 +12,40 @@ public class EnemySpawner : MonoBehaviour {
     public float height;
 
     public Transform[] asteroids;
-    public float chancePerSecond = 1;
+    public float asteroidChancePerSecond = 1;
     public float asteroidTorqueMin = -50;
     public float asteroidTorqueMax = 50;
-    public Vector2 asteroidForceMin = new Vector2(-200, 200);
-    public Vector2 asteroidForceMax = new Vector2(200, 500);
+    public Vector2 asteroidForceMin = new Vector2(-300, -200);
+    public Vector2 asteroidForceMax = new Vector2(300, 500);
     public float asteroidSizeMin = 1;
     public float asteroidSizeMax = 3;
+    public float asteroidDuration = 10;
+    public State asteroidNextState = State.GREEN_UFO;
 
-	// Use this for initialization
-	void Start () {
+    public Transform greenUFO;
+    public float greenUFOChancePerSecond = 1;
+    public float greenUFODuration = 10;
+    public State greenUFONextState = State.IDLE;
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		switch (state)
+        // Update time
+        timePassedSinceLastStateChange += Time.deltaTime;
+
+        switch (state)
         {
             case State.ASTEROID:
-                if (Random.value < chancePerSecond * Time.deltaTime)
+                if (Random.value < asteroidChancePerSecond * Time.deltaTime)
                 {
                     // Spawn asteroid
                     Transform newAsteroid = (Transform) Instantiate(RandomSelect(asteroids), new Vector3(
                         Random.Range(transform.position.x - width / 2, transform.position.x + width / 2),
-                        transform.position.y + height / 2, enemyZ), Quaternion.Euler(0, 0, Random.Range(0, 360)));
+                        transform.position.y + height, enemyZ), Quaternion.Euler(0, 0, Random.Range(0, 360)));
 
                     float size = Random.Range(asteroidSizeMin, asteroidSizeMax);
                     newAsteroid.localScale = new Vector3(size, size, 1);
@@ -43,6 +54,30 @@ public class EnemySpawner : MonoBehaviour {
                     rb.AddForce(new Vector2(Random.Range(asteroidForceMin.x, asteroidForceMax.x), Random.Range(asteroidForceMin.y, asteroidForceMax.y)));
                     rb.AddTorque(Random.Range(asteroidTorqueMin, asteroidTorqueMax));
                 }
+
+                // Change state if time up
+                if (timePassedSinceLastStateChange >= asteroidDuration)
+                {
+                    ChangeState(asteroidNextState);
+                }
+
+                break;
+
+            case State.GREEN_UFO:
+                if (Random.value < greenUFOChancePerSecond * Time.deltaTime)
+                {
+                    // Spawn green UFO
+                    Transform newGreenUFO = (Transform) Instantiate(greenUFO, new Vector3(
+                        Random.Range(transform.position.x - width / 2, transform.position.x + width / 2),
+                        transform.position.y + height, enemyZ), Quaternion.Euler(0, 0, Random.Range(0, 360)));
+                }
+
+                // Change state if time up
+                if (timePassedSinceLastStateChange >= greenUFODuration)
+                {
+                    ChangeState(greenUFONextState);
+                }
+
                 break;
         }
 	}
@@ -50,11 +85,19 @@ public class EnemySpawner : MonoBehaviour {
     void OnDrawGizmos()
     {
         Gizmos.color = Color.cyan;
-        Gizmos.DrawWireCube(transform.position, new Vector3(width, height, 1));
+        Gizmos.DrawLine(new Vector3(-width / 2, height, 0), new Vector3(width / 2, height, 0));
     }
 
     Object RandomSelect(Object[] array)
     {
         return array[Random.Range(0, array.Length - 1)];
+    }
+
+    void ChangeState(State newState)
+    {
+        state = newState;
+
+        // Reset time
+        timePassedSinceLastStateChange = 0;
     }
 }
