fc65828a382ff0573ae7f1485dee58c1c7c46120
CHEN YU <scheduledx@gmail.com>
2017-07-08 21:57:26 +0800

    Added LaserBeam

---
+++ Assets/Scripts/LaserBeamScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserBeamScript : MonoBehaviour {
+
+    //=== Public Variables ====
+    public float Speed;
+	
+	
+	//=== Private Variables ====
+	
+	
+	
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+}
