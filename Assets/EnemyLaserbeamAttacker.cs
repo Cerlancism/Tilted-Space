bc16c4554e9c5f7e610d9459bd5c78ef818522d1
IgnisIncendio <IgnisIncendio@protonmail.com>
2017-07-09 22:16:25 +0800

    Add enemy shooting and phases

---
+++ Assets/EnemyLaserbeamAttacker.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLaserbeamAttacker : MonoBehaviour {
+    public Transform enemyLaserbeam;
+    public Transform target;
+    public float chancePerSecond = 1;
+    public float inaccuracy = 20;
+
+	// Use this for initialization
+	void Start () {
+        if (!target)
+            target = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Random.value < chancePerSecond * Time.deltaTime)
+        {
+            // Calculate angle
+            Vector2 displacement = target.position - transform.position;
+            float angle = Mathf.Atan2(displacement.y, displacement.x) * Mathf.Rad2Deg - 90;
+            angle += Random.Range(-inaccuracy / 2, inaccuracy / 2);
+            
+            // Spawn enemy projectile
+            Transform newLaserbeam = Instantiate(enemyLaserbeam, transform.position, Quaternion.Euler(0, 0, angle));
+        }
+	}
+}
---
+++ Assets/EnemyProjectile.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyProjectile : MonoBehaviour {
+    public float speed = 5;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(new Vector2(0, speed * Time.deltaTime));
+	}
+}
